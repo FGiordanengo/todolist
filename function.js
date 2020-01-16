ca712c40adac1c642515ea79875c4f6e45d8593e
FGiordanengo <florent.giordanengo@gmail.com>
2020-01-16 16:46:57 +0100

    v2

---
+++ function.js
@@ -0,0 +1,18 @@
+function processAdding(){
+
+    var labelItem = document.querySelector(".item-label");
+    var newItemValue = labelItem.value;
+
+    if(newItemValue.length > 0){
+        // ITEM ET PUSH INTO LIST
+        var newItem = {};
+        newItem.label = newItemValue;
+        var countItem = list.push(newItem);
+
+        // VIEW PROCESSING
+        valueList.insertAdjacentHTML('beforebegin','<li class="listElement" id="'+countItem+'"><span class="numberList">'+ countItem +'</span>'+ newItem.label +'</li>');
+
+        // UPDATE TOTAL ITEM
+        valueCounter.innerHTML = countItem;
+    }
+}
--- index.js
+++ index.js
@@ -1,16 +1,36 @@
-var List = [
-{
-	label:"Ananas", status:"suppr", important:"1"
-},
-{
-	label:"Banane", status:"suppr", important:"1"
-},
-{
-	label:"Pomme", status:"suppr", important:"1"}];
+var list = [
+	{
+		label:"Ananas", status:"suppr", important:"1"
+	},
+	{
+		label:"Banane", status:"suppr", important:"1"
+	},
+	{
+		label:"Pomme", status:"suppr", important:"1"
+	}
+];
+	
+	
+var valueList = document.querySelector(".list");
+var i = 1;
 
+for (var valeur of list) {
+	valueList.insertAdjacentHTML('beforebegin','<li class="listElement" id="'+i+'"><span class="numberList">'+ i +'</span>'+ valeur.label +'</li><span class="hr"></span>');
+	console.log("valueList");
+	var counter = i++;
+};
 
-	for (var valeur of List) {
-		var valueList = document.querySelector(".list");
-		valueList.insertAdjacentHTML('afterbegin','<li>'+ valeur.label +'</li>');
-		console.log("valueList");
-	};
+var valueCounter= document.querySelector(".number");
+valueCounter.innerHTML = counter;
+
+var formItem = document.querySelector(".form");
+formItem.addEventListener("submit",function(e){
+	e.preventDefault();
+	processAdding();
+	formItem.reset();
+});
+
+var selectItem = document.querySelector("#")
+
+// console.log(formItem);
+	
